31c244ba7dcc72de33b85448adf3f7b4aa60f402
philwilliammee <philliammee@yahoo.com>
2026-08-04 08:05:38 -0400

    Re-vendor aisei-site: masthead containment, hero full-bleed, card overlay
    Per aisei-site commit 001a3d3.

--- blueprints/aisei-site/app/client/app/shared/layout/masthead/masthead.component.ts
+++ blueprints/aisei-site/app/client/app/shared/layout/masthead/masthead.component.ts
@@ -6,14 +6,15 @@ import { RouterLink } from '@angular/router';
   imports: [RouterLink],
   template: `
     <div class="cu-masthead">
-      <a class="brand" routerLink="/">
-        <small>Cornell University</small>
-        <strong>AI Innovation Hub</strong>
-      </a>
-      <img class="seal" src="assets/cornell_seal.svg" alt="Cornell University seal">
-      <button class="search-toggle" type="button" aria-label="Search">
-        <span aria-hidden="true">&#128269;</span>
-      </button>
+      <div class="container-fluid">
+        <div class="masthead-inner">
+          <a class="brand" routerLink="/">
+            <small>Cornell University</small>
+            <strong>AI Innovation Hub</strong>
+          </a>
+          <img class="seal" src="assets/cornell_seal.svg" alt="Cornell University seal">
+        </div>
+      </div>
     </div>
   `,
   styleUrls: ['./masthead.component.scss'],
